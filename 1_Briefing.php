9ff6524ab899d5f2a048d6f8f5ecdd3ef2247333
Aan Kristanto Nugroho <aankristanto1@gmail.com>
2022-08-30 11:16:54 +0700

    adding variable dan tipedata

--- 1_Briefing.php
+++ 1_Briefing.php
@@ -9,6 +9,7 @@ PHP adalah software Open Source, bebas untuk diunduh dan digunakan.
 File PHP dapat berisi teks, tag HTML, dan Script. File PHP dikembalikan ke browser dalam bentuk plain HTML. File PHP dapat berekstensi .php, .php3, atau .phtml.
 Kelebihan PHP sebagai bahasa script adalah : dapat dijalankan pada berbagai platform (windows, linux, dll), kompatibel terhadap hampir semua server yang digunakan saat ini, bebas diunduh dari situs resmi PHP www.php.net, mudah dipelajari dan berjalan dengan efisien pada sisi server.
 Untuk menggunakan PHP harus menginstall terlebih dahulu web server Apache (atau IIS) pada komputer/server yang akan digunakan, serta menginstall PHP dan MySQL. Atau dapat menggunakan layanan hosting yang menyediakan dukungan terhadap PHP dan MySQL
+Struktur dasar dari php yaitu menggunakan tag pembuka <?php dan tag penutup ?>
 
 */
 
