361651a64bba5bce1ca4dc837a41bd71e9ac76e3
Aan Kristanto Nugroho <aankristanto1@gmail.com>
2022-08-30 11:31:35 +0700

    edit materi 2

--- 2_Variable_Tipedata_Operator.php
+++ 2_Variable_Tipedata_Operator.php
@@ -5,16 +5,11 @@ $contohvariable = 'Hello';
 $namalengkap    = 'Universitas Ngudi Waluyo';
 
 
+
 // Setiap Variable mempunyai value dengan tipe data yang berbeda-beda terdiri atas string, integer, float, boolean, array, Object
-
-// Contoh Tipe Data String
-$namakamu = 'Ferdy Sambo';
-
-// Contoh Tipe Data Integer
-$notelp = 822495585202;
-
-// Contoh Tipe Data Float;
-$kordinatgps = 110.408739;
+$namakamu       = 'Ferdy Sambo';  // Contoh Tipe Data String
+$notelp         = 822495585202;  // Contoh Tipe Data Integer
+$kordinatgps    = 110.408739;   // Contoh Tipe Data Float;
 
 // Contoh Tipe Data Boolean yang valuenya hanya terdiri atas 0 (False) dan 1 (True)
 $varbenar = 1;
@@ -22,5 +17,11 @@ $varsalah = 0;
 
 // Contoh Tipe Data Array, menggunakan fungsi array() untuk set data sebagai array
 $mobilku = array("Innova", "Avanza", "Pajero", "Fortuner");
+
+
+
+
+
+
 
 ?>
