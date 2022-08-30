9f7aa68318e4443cd757e05de4c2eec8186c08c1
Aan Kristanto Nugroho <aankristanto1@gmail.com>
2022-08-30 11:29:49 +0700

    rename file

---
+++ 2_Variable_Tipedata_Operator.php
@@ -0,0 +1,26 @@
+<?php
+
+// Variable dalam Pemrograman PHP menggunakan karakter awal yaitu tanda $
+$contohvariable = 'Hello';
+$namalengkap    = 'Universitas Ngudi Waluyo';
+
+
+// Setiap Variable mempunyai value dengan tipe data yang berbeda-beda terdiri atas string, integer, float, boolean, array, Object
+
+// Contoh Tipe Data String
+$namakamu = 'Ferdy Sambo';
+
+// Contoh Tipe Data Integer
+$notelp = 822495585202;
+
+// Contoh Tipe Data Float;
+$kordinatgps = 110.408739;
+
+// Contoh Tipe Data Boolean yang valuenya hanya terdiri atas 0 (False) dan 1 (True)
+$varbenar = 1;
+$varsalah = 0;
+
+// Contoh Tipe Data Array, menggunakan fungsi array() untuk set data sebagai array
+$mobilku = array("Innova", "Avanza", "Pajero", "Fortuner");
+
+?>
